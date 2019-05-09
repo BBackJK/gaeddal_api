ced8949b5f74c9c2622e7add772dbce7d05267b5
BBackJK <wnsrud6839@gmail.com>
2019-05-09 10:18:58 +0900

    Add delete follow

--- src/controllers/follow/index.js
+++ src/controllers/follow/index.js
@@ -2,10 +2,12 @@ import post from './postFollow';
 import get from './getFollow';
 import getAll from './getFollows';
 import put from './putFollow';
+import remove from './deleteFollow';
 
 export default {
     post,
     get,
     getAll,
     put,
+    remove
 }
--- src/routes/follow/index.js
+++ src/routes/follow/index.js
@@ -4,6 +4,9 @@ import postFollow from './postFollow';
 import getFollow from './getFollow';
 import getAllFollow from './getFollows';
 import putFollow from './putFollow';
+import deleteFollow from './deleteFollow';
+
+import { Util } from '../../util';
 
 const router = express.Router();
 
@@ -11,5 +14,6 @@ router.get('/:id', getAllFollow);
 router.get('/my/:id', getFollow);
 router.post('/', postFollow);
 router.put('/', putFollow);
+router.delete('/:id', Util.isLoggedIn, deleteFollow);
 
 export default router;
