43dd99a6c6af6827eaa3353551bbbc099d9888e8
BBackJK <wnsrud6839@gmail.com>
2019-05-23 15:52:47 +0900

    Clean up the code and simplification

--- src/controllers/follow/deleteFollow.js
+++ src/controllers/follow/deleteFollow.js
@@ -1,32 +1,18 @@
 import { Follow } from '../../models';
-import db from '../../db/db';
 
 export default async (decodeData, paramData) => {
-  const { Op } = db.Sequelize;
-
-  const whereData = {
-    acceptanced: 1,
-    removed: 0,
-    [Op.and]: [
-      { [Op.or]: [{ follow_id: decodeData.id }, { follow_id: decodeData.id }] },
-      { [Op.or]: [{ target_id: paramData.id }, { target_id: paramData.id }] },
-    ],
-  };
-
+  const whereData = {};
   const updateData = {};
 
+  whereData.removed = 0;
+  whereData.id = paramData.id;
+
   updateData.removed = 1;
-  updateData.acceptanced = 0;
-  updateData.acceptanced_at = null;
   updateData.removed_at = new Date();
 
-  const updateResult = await Follow.update(updateData, { where: whereData });
-
-  if (updateResult[0] === 0) return null;
+  await Follow.update(updateData, { where: whereData });
 
   whereData.removed = 1;
-  whereData.acceptanced = 0;
-  whereData.acceptanced_at = null;
 
   const result = await Follow.findOne({ where: whereData });
 
